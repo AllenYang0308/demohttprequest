d8f5fb7c4ecb22a03e8ea5f68b2b831cb7fde5ad
Allen Yang <musasi.yang@gmail.com>
2023-08-25 21:21:39 +0800

    refactor: 重構http request主程式

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,27 +2,61 @@ package org.example;
 
 import org.example.authentication.Authenticate;
 import org.example.authentication.Properties;
-import org.example.connection.Connection;
+import org.example.connection.Certification;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try {
+            String apiURL = args[1];
+            String method = args[0];
+            String postData = args[2];
+            if (args.length>3) {
+                new Certification(args[3]);
+            }
+            URL url = new URL(apiURL);
+            Properties properties = new Properties();
+            properties.setUsername("<username>");
+            properties.setPassword("<password>");
+            Authenticate authenticate = new Authenticate(properties);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        Properties properties = new Properties();
-        properties.setUsername("<username>");
-        properties.setPassword("<password>");
+            connection.setRequestProperty("Authorization", authenticate.getBasicAuthenticationCredential());
+            connection.setRequestMethod(method);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/json");
 
-        Authenticate authenticate = new Authenticate(properties);
+            boolean isMatch = postData.equalsIgnoreCase("");
+            if (isMatch) {
+                System.out.println("No parameters.");
+            } else {
+                DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                outputStream.writeBytes(postData);
+                outputStream.flush();
+                outputStream.close();
+            }
 
-        Connection conn = new Connection(
-                "<target url>",
-                authenticate,
-                "GET",
-                "<certificate path>"
-        );
-        System.out.println(conn.GetResponseCode());
-        conn.PrintResponse();
-        conn.ClsoeConnection();
+            int responseCode = connection.getResponseCode();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            reader.close();
+
+            System.out.println("Response: " + response.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
