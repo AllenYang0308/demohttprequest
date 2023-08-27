7334c6595bffc9c34910b9f72b16a727038abde6
Allen Yang <musasi.yang@gmail.com>
2023-08-27 09:21:48 +0800

    refactor: 改用設定檔方式進行認證

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,10 @@ import org.example.connection.ApisConnection;
 public class Main {
     public static void main(String[] args) throws IOException {
         ApisConnection apisConnection = new ApisConnection(args);
-        apisConnection.properties.setUsername("<username>");
-        apisConnection.properties.setPassword("<password>");
+        Settings settings = new Settings("./settings.yaml");
+        UserSettings userSettings = settings.getUserSettings();
+        apisConnection.properties.setUsername(userSettings.userName);
+        apisConnection.properties.setPassword(userSettings.passWord);
         apisConnection.setRequestAuthenticate("BasicAuthentication");
         apisConnection.setRequestProperty("Content-Type", "application/json");
         apisConnection.GetApisResult();
